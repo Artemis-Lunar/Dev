24ae85690deddbac7046063a1ea956c4014c8c1f
zhz <zhanghongzhen575@gmail.com>
2026-07-30 10:40:15 +0800

    add  for loop for 0-10

--- src/main.java
+++ src/main.java
@@ -2,6 +2,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int i = 0;
+        for(i = 0 ;i <=10; i++){
+            System.out.println(i);
+        }
     }
 }
 
